320e1715d955996f663c0580921b372ef8aad730
SaeedehDeldar <saeedeh.deldar@gmail.com>
2021-05-19 10:31:26 +0200

    .

--- app/app.jsx
+++ app/app.jsx
@@ -3,26 +3,12 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { StyleSheetManager } from "styled-components";
 import { AppStyled } from "./app.styled";
-import { Provider } from "./context";
-import reducer from "./reducer";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, []);
-
-  useEffect(() => {
-    console.log(`[STATE]`, { ...state });
-  }, [state]);
-
   return (
-    <Provider
-      value={useMemo(() => {
-        return { state, dispatch };
-      }, [state])}
-    >
-      <AppStyled>
-        <div>phone book</div>
-      </AppStyled>
-    </Provider>
+    <AppStyled>
+      <div>phone book</div>
+    </AppStyled>
   );
 };
 
